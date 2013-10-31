2b4661f92787a868f560b72afe9275dbd17302f4
Lasse Bunk <lassebunk@gmail.com>
2013-10-31 14:10:34 +0100

    Add with_breadcrumb_trail

--- lib/gretel/trails/patches/renderer.rb
+++ lib/gretel/trails/patches/renderer.rb
@@ -12,6 +12,20 @@ Gretel::Renderer.class_eval do
 
   # Returns encoded trail for the breadcrumb.
   def trail
-    @trail ||= Gretel::Trails.encode(links)
+    @trail ||= begin
+      transformed_links = links.dup
+      if transform_current_path && transformed_links.any? && request
+        transformed_links.last.url = request.fullpath
+      end
+      Gretel::Trails.encode(transformed_links)
+    end
   end
+
+  # Whether to set the current link path to +request.fullpath+.
+  def transform_current_path
+    return @transform_current_path if defined?(@transform_current_path)
+    @transform_current_path = true
+  end
+
+  attr_writer :transform_current_path
 end
--- lib/gretel/trails/patches/view_helpers.rb
+++ lib/gretel/trails/patches/view_helpers.rb
@@ -3,4 +3,12 @@ Gretel::ViewHelpers.class_eval do
   def breadcrumb_trail
     gretel_renderer.trail
   end
+
+  # Yields a block having the breadcrumb trail set to the breadcrumb given by +key+.
+  def with_breadcrumb_trail(key, *args, &block)
+    with_breadcrumb(key, *args) do
+      gretel_renderer.transform_current_path = false
+      yield
+    end
+  end
 end
